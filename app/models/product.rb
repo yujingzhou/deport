777418054634d3bd80c7c962ba2216e453b65937
robbert <yujingzhou2010@gmail.com>
2011-04-12 12:05:12 +0800

    update product validates

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,13 +3,11 @@ class Product < ActiveRecord::Base
   validates :description, :presence =>true
   validates :image_url, :presence =>true
   validates :price , :presence =>true,
-            :numericality =>{:message =>'must be a numer'}
+                     :numericality =>{:message =>'must be a numer'}
+  validates :image_url, :format => { :with =>%r{\.(gif|jpg|png)$}i, :message =>'must be a url for gif,jpg or png image'}
   validate :price_must_be_at_least_a_acent
-  validates_format_of:image_url,
-                     :with =>%r{\.(gif|jpg|png)$}i,
-                     :message =>'must be a url for gif,jpg or png image'
   protected  
-    def price_must_be_at_least_a_acent
-      errors.add(:price,'should be at least 0.01') if price.nil?|| price<0.01
-    end
+  def price_must_be_at_least_a_acent
+    errors.add(:price,'should be at least 0.01') if price.nil?|| price<0.01
+  end
 end

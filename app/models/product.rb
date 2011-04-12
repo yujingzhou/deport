aa119d28c673cdb04be91b5947fe1dba9f5ad041
robbert <yujingzhou2010@gmail.com>
2011-04-12 13:44:30 +0800

    update for validates-uniquess

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,6 +6,7 @@ class Product < ActiveRecord::Base
                      :numericality =>{:message =>'must be a numer'}
   validates :image_url, :format => { :with =>%r{\.(gif|jpg|png)$}i, :message =>'must be a url for gif,jpg or png image'}
   validate :price_must_be_at_least_a_acent
+  
   protected  
   def price_must_be_at_least_a_acent
     errors.add(:price,'should be at least 0.01') if price.nil?|| price<0.01

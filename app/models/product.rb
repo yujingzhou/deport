678b85948a9e1c327e328d41e256a6d92acccf8a
robbert <yujingzhou2010@gmail.com>
2011-04-14 11:01:37 +0800

    update for carts add produts

--- app/models/product.rb
+++ app/models/product.rb
@@ -11,4 +11,23 @@ class Product < ActiveRecord::Base
   def price_must_be_at_least_a_acent
     errors.add(:price,'should be at least 0.01') if price.nil?|| price<0.01
   end
+  default_scope :order => 'title'
+  has_many :line_items
+  #before_destroy :ensure_not_referenced_by_any_line_item
+  # ensure that there are no line items referencing this product
+ # def ensure_not_referenced_by_any_line_item
+    before_destroy :ensure_not_referenced_by_any_line_item
+    # ensure that there are no line items referencing this product
+    def ensure_not_referenced_by_any_line_item
+    if line_items.count.zero?
+      return true
+    else
+      errors[:base] << "Line Items present"  #这是什么意思
+      return false
+    end
+  end
 end
+
+#before_destroy :ensure_not_referenced_by_any_line_item
+# ensure that there are no line items referencing this product
+#def ensure_not_referenced_by_any_line_item
